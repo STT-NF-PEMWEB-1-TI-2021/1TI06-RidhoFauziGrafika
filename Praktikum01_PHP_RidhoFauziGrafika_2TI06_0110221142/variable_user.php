fcceabc05e10a1c6cf681e60379cd29ca066506a
RidhoFauziGrafika <92265091+RidhoFauziGrafika@users.noreply.github.com>
2022-02-26 11:30:56 +0700

    Add files via upload

---
+++ Praktikum01_PHP_RidhoFauziGrafika_2TI06_0110221142/variable_user.php
@@ -0,0 +1,15 @@
+<?php 
+// definisikan variable
+
+$nama = 'Rosalie Naurah';
+$umur = 13;
+$berat = 22.4;
+
+echo 'Nama : ' . $nama;
+echo '<br> Umur : ' . $umur.' Tahun';
+echo '<br> Berat : ' . $berat,' Kg';
+
+echo "<br>Hallo $nama Apa kabar";
+
+
+?>
